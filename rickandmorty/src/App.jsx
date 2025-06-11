1a095ab218aa788d4fe3073555be3006409fea41
NemanjaStankovic <nemanjast@elfak.rs>
2025-06-11 22:54:55 +0200

    infinite scroll

--- rickandmorty/src/App.jsx
+++ rickandmorty/src/App.jsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 
 function App() {
     const [selectedCharacters, setSelectedCharacters] = useState([]);
     const [statusFilter, setStatusFilter] = useState('');
     const [nameFilter, setNameFilter] = useState('');
+    const [page, setPage] = useState(1);
+    const [hasMorePages, setHasMorePages] = useState(true);
+
+    useEffect(() => {
+        setSelectedCharacters([]);
+        setPage(1);
+    }, [statusFilter, nameFilter]);
     useEffect(() => { console.log(nameFilter) }, [nameFilter]);
     useEffect(() => {
         const url = 'https://rickandmortyapi.com/api/character';
@@ -14,19 +21,34 @@ function App() {
         if (nameFilter) {
             urlParams.append('name', nameFilter);
         }
+        urlParams.append('page', page);
+
         fetch(url + `?${urlParams.toString()}`)
             .then(res => res.json())
             .then(data => {
-                setSelectedCharacters(data.results);
+                setSelectedCharacters(prev => [...prev, ...data.results]);
+                setHasMorePages(data.info.next != null);
             })
-    }, [statusFilter, nameFilter]);
+    }, [statusFilter, nameFilter, page]);
+
+    const observer = useRef();
+    const lastCharacterRef = useCallback(
+        node => {
+            if (observer.current) observer.current.disconnect();
+            observer.current = new IntersectionObserver(entries => {
+                if (entries[0].isIntersecting && hasMorePages) {
+                    setPage(prevPage => prevPage + 1);
+                }
+            });
+            if (node) observer.current.observe(node);
+        }, [hasMorePages]);
     return (
         <>
             <Filter statusFilter={statusFilter} setStatusFilter={setStatusFilter}></Filter>
             <Search nameFilter={nameFilter} setNameFilter={ setNameFilter}></Search>
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 p-4">
-              {selectedCharacters?selectedCharacters.map(character => (
-                  <div key={character.id} class="relative group cursor-pointer">
+                {selectedCharacters ? selectedCharacters.map((character, index) => (
+                    <div key={character.id} ref={index===selectedCharacters.length - 1? lastCharacterRef: null} class="relative group cursor-pointer">
                       <div class="shadow m-1 p-2 rounded-2xl flex max-w-xs overflow-auto transition-all duration-300 max-h-[400px] bg-white custom-scrollbar">
                           <div>
                               <h1 class="font-bold text-xl mb-2">{character.name}</h1>
